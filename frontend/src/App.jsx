b1c7e50219ff84e4c8cdb1556c5d63dfba54906f
SE192843 <nguyenquocbao0704482532@gmail.com>
2026-06-03 20:51:08 +0700

    CardPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './components/LoginPage'
 import DashboardShell from './components/DashboardShell'
 import './App.css'
+import CardPage from './components/CardPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/login/dashboard/*" element={<DashboardShell />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/login" element={<LoginPage />} />
+    //   <Route path="/login/dashboard/*" element={<DashboardShell />} />
+    //   <Route path="*" element={<Navigate to="/login" replace />} />
+    // </Routes>
+    <>
+      <CardPage />
+    </>
   )
 }
