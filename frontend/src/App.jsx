079ab29777092aaa6e93f3c10fc8724c25c1ab0e
hunter-tran <huntertran0703@gmail.com>
2026-06-05 13:41:22 +0700

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './components/LoginPage'
 import DashboardShell from './components/DashboardShell'
 import './App.css'
+import GeneralStatisticsTable from './components/Generalstatisticstable'
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/login/dashboard/*" element={<DashboardShell />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/login" element={<LoginPage />} />
+    //   <Route path="/login/dashboard/*" element={<DashboardShell />} />
+    //   <Route path="*" element={<Navigate to="/login" replace />} />
+    // </Routes>
+    <><GeneralStatisticsTable /></>
   )
 }
